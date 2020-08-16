7b62a0488f05c3ec800c784798354b9cc788cbf4
Hnooz <mohammededris23@gmail.com>
2020-08-16 21:57:04 +0200

    start

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app" class="">
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style src="./assets/css/app.css">
+</style>
