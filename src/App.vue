125bb1f2286f16d3a1f87efd992faeecdceebe0d
Hnooz <mohammededris23@gmail.com>
2020-08-18 16:23:02 +0200

    fix some issus

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,19 @@
 <template>
   <div class="container max-w-md mx-auto"> 
-      <div class="bg-gray-800 row">
-        <div class="flex items-center justify-between pt-5 px-4">
-          <h1 class="text-gray-600 font-semibold">Whatsapp</h1>
-          <div class="flex text-gray-600">
-            <i class="px-4">
-              <svg viewBox="0 0 20 20" fill="currentColor" class="search w-4 h-4"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
-            </i>
-            <i>
-              <svg viewBox="0 0 20 20" fill="currentColor" class="dots-vertical w-4 h-4"><path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z"></path></svg>
-            </i>
-          </div>
-        </div>
-        <div class="pt-3 flex justify-between">
-
-          <div class="border-b-2 border-teal-500 text-gray-500 w-1/3 pb-3">
-            <router-link class="px-10" to="/" >Chats</router-link>
-          </div>
-          
-          <div class="border-b-2 border-teal-500 text-gray-500 w-1/3 pb-3">
-            <router-link class="px-10" to="/status">Status</router-link>
-          </div>
-          
-          <div class="border-b-2 border-teal-500 text-gray-500 w-1/3 pb-3">
-            <router-link class="px-10" to="/calls">Calls</router-link>
-          </div>
-        </div>
-      </div> 
-
+      <Nav/>
       <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Nav from '../src/components/Nav.vue'
 export default {
-  data() {
-    return {
-      chats : true,
-      status: false,
-      calls: false
-    }
-  },
-
+  components:{
+    Nav
+  }
 }
 </script>
 
-<style src="./assets/css/app.css">
+<style src="./assets/css/app.css" scopeed>
+
 </style>
--- src/views/Chats.vue
+++ src/views/Chats.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="">
-    <div class="mt-3 flex" v-for="(count, index) in counts" :key="index">
+    <div class="mt-3 flex" v-for="(pepole, index) in pepoles" :key="index">
       <div class="bg-center bg-cover flex h-12 items-center justify-center mx-4 photo px-5 rounded-full w-12">
         <!-- <img src="../assets/img/profile.jpg" alt="" class="w-16 rounded-full mx-4 bg-cover"> -->
       </div>
       <div class="flex justify-between w-full border-b border-gray-800 mr-4 pb-3">
         <div class="">
-          <h1 class="font-semibold text-gray-400">name</h1>
-          <span class="font-light text-gray-600">message</span>
+          <h1 class="font-semibold text-gray-400" >{{pepole.name}}</h1>
+          <span class="font-light text-gray-600" v-if="pepole.message.length > 20">{{pepole.message.substr(0,20)}}...</span>
+           <span class="font-light text-gray-600" v-else>{{pepole.message}}</span>
         </div>
         <div class="">
-          <p class="font-bold text-teal-500">2:90 PM</p>
+          <p class="font-bold text-teal-500">{{pepole.time}}</p>
           <div class="flex items-center">
             <svg viewBox="0 0 20 20" fill="currentColor" class="volume-off mr-1 text-gray-500 w-6 h-6"><path fill-rule="evenodd" d="M9.383 3.076A1 1 0 0110 4v12a1 1 0 01-1.707.707L4.586 13H2a1 1 0 01-1-1V8a1 1 0 011-1h2.586l3.707-3.707a1 1 0 011.09-.217zM12.293 7.293a1 1 0 011.414 0L15 8.586l1.293-1.293a1 1 0 111.414 1.414L16.414 10l1.293 1.293a1 1 0 01-1.414 1.414L15 11.414l-1.293 1.293a1 1 0 01-1.414-1.414L13.586 10l-1.293-1.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg> 
             <span class="bg-teal-500 flex h-5 items-center justify-center rounded-full text-xs w-5">16</span></div>
@@ -32,9 +33,18 @@ export default {
 
   data() {
     return {
-      counts:7
+      counts:7,
+      pepoles: [
+        {name:'hnooz',message:'i need some food',time:'20:10 PM'},
+        {name:'Guest',message:'lemon lemon',time:'20:10 PM'},
+        {name:'Khatab',message:'f*ck sudan',time:'20:10 AM'},
+        {name:'Tester',message:'you are good',time:'20:10 PM'},
+        {name:'Taz',message:'listen to this and give me opinion',time:'20:10 AM'},
+      ]
     }
   },
+
+
 }
 </script>
 
