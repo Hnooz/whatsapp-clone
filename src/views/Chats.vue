a05ea3e2ecbe0bdaab56562e7145f965937071ec
Hnooz <mohammededris23@gmail.com>
2020-08-19 18:42:30 +0200

    add status

--- src/views/Chats.vue
+++ src/views/Chats.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="">
     <div class="mt-3 flex" v-for="(pepole, index) in pepoles" :key="index">
-      <div class="bg-center bg-cover flex h-12 items-center justify-center mx-4 photo px-5 rounded-full w-12">
+      <div class="bg-center bg-cover flex items-center justify-center mx-4 px-5 rounded-full w-12 h-12 photo ">
         <!-- <img src="../assets/img/profile.jpg" alt="" class="w-16 rounded-full mx-4 bg-cover"> -->
       </div>
       <div :class="pepoles.length != (index + 1) ?'flex justify-between w-full border-b border-gray-800 mr-4 pb-3' : 'flex justify-between w-full mr-4 pb-3'">
@@ -12,9 +12,9 @@
         </div>
         <div class="">
           <p class="font-bold text-teal-500">{{pepole.time}}</p>
-          <div class="flex items-center">
-            <svg viewBox="0 0 20 20" fill="currentColor" class="volume-off mr-1 text-gray-500 w-6 h-6"><path fill-rule="evenodd" d="M9.383 3.076A1 1 0 0110 4v12a1 1 0 01-1.707.707L4.586 13H2a1 1 0 01-1-1V8a1 1 0 011-1h2.586l3.707-3.707a1 1 0 011.09-.217zM12.293 7.293a1 1 0 011.414 0L15 8.586l1.293-1.293a1 1 0 111.414 1.414L16.414 10l1.293 1.293a1 1 0 01-1.414 1.414L15 11.414l-1.293 1.293a1 1 0 01-1.414-1.414L13.586 10l-1.293-1.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg> 
-            <span class="bg-teal-500 flex h-5 items-center justify-center rounded-full text-xs w-5">16</span></div>
+          <div :class="pepole.mute == true? 'pl-2' : ''"  class="flex items-center">
+            <svg v-show="pepole.mute == true" viewBox="0 0 20 20" fill="currentColor" class="volume-off mr-1 text-gray-500 w-6 h-6"><path fill-rule="evenodd" d="M9.383 3.076A1 1 0 0110 4v12a1 1 0 01-1.707.707L4.586 13H2a1 1 0 01-1-1V8a1 1 0 011-1h2.586l3.707-3.707a1 1 0 011.09-.217zM12.293 7.293a1 1 0 011.414 0L15 8.586l1.293-1.293a1 1 0 111.414 1.414L16.414 10l1.293 1.293a1 1 0 01-1.414 1.414L15 11.414l-1.293 1.293a1 1 0 01-1.414-1.414L13.586 10l-1.293-1.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg> 
+            <span :class="pepole.mute == false? 'ml-10' : ''" class="bg-teal-500 flex h-5 items-center justify-center rounded-full text-xs w-5">{{pepole.msgCount}}</span></div>
         </div>
       </div>
     </div>
@@ -30,11 +30,11 @@ export default {
   data() {
     return {
       pepoles: [
-        {name:'hnooz',message:'i need some food',time:'20:10 PM'},
-        {name:'Guest',message:'lemon lemon',time:'20:10 PM'},
-        {name:'Khatab',message:'f*ck sudan',time:'20:10 AM'},
-        {name:'Tester',message:'you are good',time:'20:10 PM'},
-        {name:'Taz',message:'listen to this and give me opinion',time:'20:10 AM'},
+        {name:'Panda',message:'i need some food',time:'20:10 PM', mute:false, msgCount:6},
+        {name:'Guest',message:'lemon lemon',time:'20:10 PM', mute:false, msgCount:8},
+        {name:'Khatab',message:'f*ck sudan',time:'20:10 AM', mute:false, msgCount:4},
+        {name:'Tester',message:'you are good',time:'20:10 PM', mute:true, msgCount:50},
+        {name:'Taz',message:'listen to this and give me opinion',time:'20:10 AM', mute:false, msgCount:9},
       ]
     }
   },
--- src/views/Status.vue
+++ src/views/Status.vue
@@ -1,5 +1,132 @@
 <template>
   <div>
-    <!-- <svg viewBox="0 0 20 20" fill="currentColor" class="dots-horizontal w-6 h-6"><path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"></path></svg> -->
+    
+    <div>
+      
+      <div class="flex items-center justify-between mr-4 mt-2">
+        <div class="flex">
+          <button class="bg-center bg-cover border-2 border-gray-300 flex focus:outline-none h-12 items-center justify-center mx-4 outline-none photo px-5 rounded-full w-12" type="button" style="transition: all .15s ease" v-on:click="toggleModal()"></button>
+          <div>
+            <h1 class="font-medium text-gray-300">My status</h1>
+            <span class="text-sm text-gray-600">Today, 12:56 AM</span>
+          </div>
+        </div>
+
+        <div>
+          <svg viewBox="0 0 20 20" fill="currentColor" class="dots-horizontal w-6 h-6 text-gray-600"><path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"></path></svg>
+        </div>
+      </div>
+    <div class="mt-2">
+      <h1 class="font-medium mx-4 my-2 text-gray-500 text-sm">Recent updates</h1>
+      <div v-for="(pepole, index) in pepoles" :key="index">
+        <div class="flex pb-3" v-show="pepole.show.recent == true">
+            <button class="bg-center bg-cover border-2 border-gray-300 flex focus:outline-none h-12 items-center justify-center mx-4 outline-none photo px-5 rounded-full w-12" type="button" style="transition: all .15s ease" v-on:click="toggleModal()"></button>
+            <div :class="pepoles.length == (index + 1) ? 'border-b border-gray-800 mr-3 pb-2 w-full' : 'mr-3 pb-2 w-full'">
+              <h1 class="font-medium text-gray-300 pl-1">{{pepole.name}}</h1>
+              <span class="text-sm text-gray-600 pl-1">{{pepole.time}}</span>
+            </div>
+        </div>
+      </div>
+      <h1 class="font-medium mx-4 my-2 text-gray-500 text-sm">Viewed updates</h1>
+
+      <div class="flex pb-3">
+            <button class="bg-center bg-cover border-2 border-gray-300 flex focus:outline-none h-12 items-center justify-center mx-4 outline-none photo px-5 rounded-full w-12" type="button" style="transition: all .15s ease" v-on:click="toggleModal()"></button>
+            <div class="border-b border-gray-800 mr-3 pb-2 w-full">
+              <h1 class="font-medium text-gray-300 pl-1">Omer</h1>
+              <span class="text-sm text-gray-600 pl-1">Today, 5:30 PM</span>
+            </div>
+        </div>
+
+      <div class="flex pb-3">
+            <button class="bg-center bg-cover border-2 border-gray-300 flex focus:outline-none h-12 items-center justify-center mx-4 outline-none photo px-5 rounded-full w-12" type="button" style="transition: all .15s ease" v-on:click="toggleModal()"></button>
+            <div class="border-b border-gray-800 mr-3 pb-2 w-full">
+              <h1 class="font-medium text-gray-300 pl-1">Panda</h1>
+              <span class="text-sm text-gray-600 pl-1">Today, 2:30 AM</span>
+            </div>
+        </div>
+
+      <div class="flex pb-3">
+            <button class="bg-center bg-cover border-2 border-gray-300 flex focus:outline-none h-12 items-center justify-center mx-4 outline-none photo px-5 rounded-full w-12" type="button" style="transition: all .15s ease" v-on:click="toggleModal()"></button>
+            <div class="border-b border-gray-800 mr-3 pb-2 w-full">
+              <h1 class="font-medium text-gray-300 pl-1">Guest</h1>
+              <span class="text-sm text-gray-600 pl-1">Yesterday, 11:30 PM</span>
+            </div>
+        </div>
+
+      <div class="flex pb-3">
+            <button class="bg-center bg-cover border-2 border-gray-300 flex focus:outline-none h-12 items-center justify-center mx-4 outline-none photo px-5 rounded-full w-12" type="button" style="transition: all .15s ease" v-on:click="toggleModal()"></button>
+            <div class="mr-3 pb-2 w-full">
+              <h1 class="font-medium text-gray-300 pl-1">Mosab</h1>
+              <span class="text-sm text-gray-600 pl-1">Yesterday, 8:30 AM</span>
+            </div>
+        </div>
+
+    </div>
+      <div v-if="showModal" class="overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center flex">
+        <div class="relative w-auto my-6 mx-auto max-w-6xl">
+        
+          <div class="relative flex flex-col w-full outline-none focus:outline-none">
+          
+            <div class="flex items-start justify-between relative rounded-t -mb-2 -mr-3">
+            
+              <button class=" ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none" @click="toggleModal()">
+                <span title="close" class="bg-transparent text-red-700 opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                  ×
+                </span>
+              </button>
+            </div>
+            
+            <div class="-m-10 p-6 flex-auto">
+            
+                <div class="bg-center bg-cover flex items-center justify-center rounded-lg w-64 h-64 photo "></div>
+            
+            </div>
+          </div>
+        </div>
+      </div>
+      <div v-if="showModal" class="opacity-25 fixed inset-0 z-40 bg-black"></div>
+    </div>
+
   </div>
 </template>
+
+<script>
+// import { Carousel, Slide } from 'vue-carousel';
+export default {
+  name: "large-modal",
+    components: {
+    // Carousel,
+    // Slide
+  },
+
+  data() {
+    return {
+      showModal: false,
+       pepoles: [
+        {name:'Panda', show:{viewed:true, recent:false},time:'20:10 PM'},
+        {name:'Guest', show:{viewed:true, recent:false},time:'20:10 PM'},
+        {name:'Khatab',show:{viewed:false,recent:true},time:'Today, 4:00 PM'},
+        {name:'Tester',show:{viewed:true, recent:false},time:'20:10 PM'},
+        {name:'Taz',   show:{viewed:false,recent:false},time:'20:10 AM'},
+      ]
+    }
+  },
+  methods: {
+    toggleModal(){
+      this.showModal = !this.showModal;
+    },
+    closeModal(){
+      this.showModal = !this.showModal;
+    }
+
+    
+  },
+
+}
+</script>
+
+<style scoped>
+.photo{
+  background-image: url('../assets/img/profile.jpg');
+}
+</style>
